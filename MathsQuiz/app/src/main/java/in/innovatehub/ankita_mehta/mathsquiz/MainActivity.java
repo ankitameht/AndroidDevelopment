5b6e4ab38e9fa01a79f0933685c4b74a794cb98b
ankita_meht <ankitamehta283@gmail.com>
2016-08-13 04:38:27 +0530

    commit 2

---
+++ MathsQuiz/app/src/main/java/in/innovatehub/ankita_mehta/mathsquiz/MainActivity.java
@@ -0,0 +1,49 @@
+package in.innovatehub.ankita_mehta.mathsquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Math Quiz";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        Log.d(TAG, "Inside OnStart");
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        Log.d(TAG,"Inside OnPause");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG,"Inside OnResume");
+
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG, "Inside OnStop");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "Inside OnDestroy");
+    }
+}
