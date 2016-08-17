2d20b36106df84acc0630cfd7c790d9b6837c2a7
ankita_meht <ankitamehta283@gmail.com>
2016-08-17 23:50:14 +0530

    FIxed Errors

--- MathsQuiz/app/src/main/java/in/innovatehub/ankita_mehta/mathsquiz/MainActivity.java
+++ MathsQuiz/app/src/main/java/in/innovatehub/ankita_mehta/mathsquiz/MainActivity.java
@@ -1,5 +1,6 @@
 package in.innovatehub.ankita_mehta.mathsquiz;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     static final String Ran_Num = "RandomNumber";
 
     /* Stores prime numbers between 1 and 1000*/
-    static final String primeNum[] = {2,   3,   5,   7,  11,  13,  17,  19,  23,  29,  31,  37,  41, 43,  47,  53,  59,  61,  67,  71,  73,  79,  83,  89,  97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997};
+    static final int primeNum[] = {2,   3,   5,   7,  11,  13,  17,  19,  23,  29,  31,  37,  41, 43,  47,  53,  59,  61,  67,  71,  73,  79,  83,  89,  97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997};
     
     /* XML variables */
     private TextView tv;
@@ -37,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         if(i<500){
             randnum = i;
         }else{
-            randnum = primeNum[(i%165)];
+            randnum = primeNum[(i%primeNum.length)];
         }
         randn = String.valueOf(randnum);
     }
@@ -71,15 +72,15 @@ public class MainActivity extends AppCompatActivity {
         b1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if(isPrime()){
-                    Toast.makeText(MainActivity.this, R.string.Correct, Toast.LENGTH_SHORT);
-                    TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
-                    v.setTextColor(Color.GREEN);
+                    Toast toast = Toast.makeText(MainActivity.this, R.string.Correct, Toast.LENGTH_SHORT);
+                    TextView tv = (TextView) toast.getView().findViewById(android.R.id.message);
+                    tv.setTextColor(Color.GREEN);
                     toast.show();
                 }
                 else{
-                    Toast.makeText(MainActivity.this, R.string.Incorrect, Toast.LENGTH_SHORT);
-                    TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
-                    v.setTextColor(Color.RED);
+                    Toast toast = Toast.makeText(MainActivity.this, R.string.Incorrect, Toast.LENGTH_SHORT);
+                    TextView tv = (TextView) toast.getView().findViewById(android.R.id.message);
+                    tv.setTextColor(Color.RED);
                     toast.show();
                 }
             }
@@ -87,15 +88,15 @@ public class MainActivity extends AppCompatActivity {
         b2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
             if(isPrime()){
-                    Toast.makeText(MainActivity.this, R.string.Correct, Toast.LENGTH_SHORT);
-                    TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
-                    v.setTextColor(Color.GREEN);
+                    Toast toast = Toast.makeText(MainActivity.this, R.string.Correct, Toast.LENGTH_SHORT);
+                    TextView tv = (TextView) toast.getView().findViewById(android.R.id.message);
+                    tv.setTextColor(Color.GREEN);
                     toast.show();
             }
             else{
-                    Toast.makeText(MainActivity.this, R.string.Incorrect, Toast.LENGTH_SHORT);
-                    TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
-                    v.setTextColor(Color.RED);
+                    Toast toast = Toast.makeText(MainActivity.this, R.string.Incorrect, Toast.LENGTH_SHORT);
+                    TextView tv = (TextView) toast.getView().findViewById(android.R.id.message);
+                    tv.setTextColor(Color.RED);
                     toast.show();
             }
         }});
