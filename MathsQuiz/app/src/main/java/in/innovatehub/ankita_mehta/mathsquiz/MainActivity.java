00c23fb6aac00293fbb7c22400a0b6b4dafcb2a5
ankita_meht <ankitamehta283@gmail.com>
2016-08-17 23:11:10 +0530

    Updating prime numbers

--- MathsQuiz/app/src/main/java/in/innovatehub/ankita_mehta/mathsquiz/MainActivity.java
+++ MathsQuiz/app/src/main/java/in/innovatehub/ankita_mehta/mathsquiz/MainActivity.java
@@ -13,37 +13,48 @@ import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
 
+    /* static text variables*/
     private static final String TAG = "Math Quiz";
     static final String Ran_Num = "RandomNumber";
 
+    /* Stores prime numbers between 1 and 1000*/
+    static final String primeNum[] = {2,   3,   5,   7,  11,  13,  17,  19,  23,  29,  31,  37,  41, 43,  47,  53,  59,  61,  67,  71,  73,  79,  83,  89,  97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997};
+    
+    /* XML variables */
     private TextView tv;
     private Button b1;
     private Button b2;
     private Button b3;
+
+    /* Other variables*/
     private int randnum;
     private String randn;
 
+    /* This function uses Random function to generate a random number, then uniformly chooses a prime number */
     public void numberToSet(){
-
         Random r = new Random();
         int i = r.nextInt(1000) + 1;
-        randnum = i;
+        if(i<500){
+            randnum = i;
+        }else{
+            randnum = primeNum[(i%165)];
+        }
         randn = String.valueOf(randnum);
-
     }
-    public void settheView(){
+
+    /* This sets the random number on screen using text view */
+    public void setTheView(){
         numberToSet();
         tv = (TextView) findViewById(R.id.numberToSet);
         tv.setText(randn);
     }
+
+    /* This function checks if generated random number is prime or not */
     public boolean isPrime(){
-
         for(int i =2; i<Math.sqrt(randnum);i++){
-
             if(randnum % i == 0){
                 return false;
             }
-
         }
         return true;
     }
@@ -52,53 +63,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        settheView();
+        setTheView();
         b1 = (Button) findViewById(R.id.TrueButtonID);
         b2 = (Button) findViewById(R.id.FalseButtonID);
         b3 = (Button) findViewById(R.id.NextButtonID);
 
         b1.setOnClickListener(new View.OnClickListener() {
-
-
             public void onClick(View v) {
-
                 if(isPrime()){
-                    Toast.makeText(MainActivity.this, R.string.Correct, Toast.LENGTH_SHORT).show();
-
+                    Toast.makeText(MainActivity.this, R.string.Correct, Toast.LENGTH_SHORT);
+                    TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
+                    v.setTextColor(Color.GREEN);
+                    toast.show();
                 }
                 else{
-                    Toast.makeText(MainActivity.this, R.string.Incorrect, Toast.LENGTH_SHORT).show();
-
+                    Toast.makeText(MainActivity.this, R.string.Incorrect, Toast.LENGTH_SHORT);
+                    TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
+                    v.setTextColor(Color.RED);
+                    toast.show();
                 }
-                }
+            }
         });
-
         b2.setOnClickListener(new View.OnClickListener() {
-
-
             public void onClick(View v) {
-
-                if(isPrime()){
-                    Toast.makeText(MainActivity.this, R.string.Incorrect, Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    Toast.makeText(MainActivity.this, R.string.Correct, Toast.LENGTH_SHORT).show();
-
-                }
+            if(isPrime()){
+                    Toast.makeText(MainActivity.this, R.string.Correct, Toast.LENGTH_SHORT);
+                    TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
+                    v.setTextColor(Color.GREEN);
+                    toast.show();
             }
-        });
-
+            else{
+                    Toast.makeText(MainActivity.this, R.string.Incorrect, Toast.LENGTH_SHORT);
+                    TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
+                    v.setTextColor(Color.RED);
+                    toast.show();
+            }
+        }});
         b3.setOnClickListener(new View.OnClickListener() {
-
-
             public void onClick(View v) {
-                settheView();
+                setTheView();
             }
         });
-
-
-
-
     }
 
     @Override
@@ -134,8 +139,6 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "Inside OnDestroy");
     }
 
-
-
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         savedInstanceState.putInt(Ran_Num, randnum);
@@ -145,7 +148,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-
         randnum = savedInstanceState.getInt(Ran_Num);
     }
 
